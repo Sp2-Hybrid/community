b95f7f9611ca98e8da355222c162e75034531ff8
sp2_hybrid <1171891648@qq.com>
2021-08-22 21:13:12 +0800

    valid_code

--- src/main/java/com/songpeng/community/controller/LoginController.java
+++ src/main/java/com/songpeng/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.songpeng.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.songpeng.community.entity.User;
 import com.songpeng.community.service.UserService;
 import com.songpeng.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 /**
@@ -18,11 +27,21 @@ import java.util.Map;
 @Controller
 public class LoginController implements CommunityConstant {
     @Autowired
+    public static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptcha;
 
     @GetMapping("/register")
     public String getRegisterPage(){
         return "/site/register";
+    }
+
+    @GetMapping(path = "/login")
+    public String getLoginPage() {
+        return "/site/login";
     }
 
     @PostMapping("/register")
@@ -45,7 +64,7 @@ public class LoginController implements CommunityConstant {
         int result = userService.activation(userId, code);
         if(result==ACTIVATION_SUCCESS){
             model.addAttribute("msg", "激活成功，您的账号已经可以正常使用了");
-            model.addAttribute("target", "/index");
+            model.addAttribute("target", "/login");
         }else if(result==ACTIVATION_REPEAT){
             model.addAttribute("msg", "无效操作,该账号已经激活过了!");
             model.addAttribute("target", "/index");
@@ -54,5 +73,25 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    @GetMapping("/kaptcha")
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        // 因为是敏感数据，所以保存到session中
+        // 生成验证码
+        String text = kaptcha.createText();
+        BufferedImage image = kaptcha.createImage(text);
+
+        // 验证码文字存入session
+        session.setAttribute("kaptcha", text);
+
+        // 将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("相应验证码失败 : " + e.getMessage());
+        }
     }
 }
